7b0539049d4c125582f81510f336ae63ac866b0a
Brandon <Apex2.0predator@gmail.com>
2025-06-09 10:58:05 +0200

    updated views

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,7 +1,9 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.urls import path, re_path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -16,7 +18,12 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
+
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include(router.urls)),
 ]
